7ceb0b2fc167b4a563f53c3f469d198a062430e0
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-11-04 11:23:29 +0100

    Added bounds to simplespritesymbol

--- format/swf/lite/symbols/SimpleSpriteSymbol.hx
+++ format/swf/lite/symbols/SimpleSpriteSymbol.hx
@@ -3,13 +3,14 @@ package format.swf.lite.symbols;
 
 import format.swf.exporters.core.ShapeCommand;
 import openfl.geom.Matrix;
+import openfl.geom.Rectangle;
 
 class SimpleSpriteSymbol extends SWFSymbol {
 
 
 	public var bitmapID:Int;
 	public var matrix:Matrix;
-
+	public var bounds:Rectangle;
 
 	public function new () {
 		super ();
